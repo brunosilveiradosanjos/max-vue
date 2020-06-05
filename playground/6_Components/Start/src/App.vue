872af57724c5f7e957b12cda500e09be00389c6b
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-06-04 23:26:54 -0300

    Assignment 6: Time to Practice - Components

---
+++ playground/6_Components/Start/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <app-server-status></app-server-status>
+      <app-server-details></app-server-details>
+    </div>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import ServerDetails from "./ServerDetails.vue";
+import ServerStatus from "./ServerStatus.vue";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-server-details": ServerDetails,
+    "app-server-status": ServerStatus
+  }
+};
+</script>
+
+<style>
+</style>
