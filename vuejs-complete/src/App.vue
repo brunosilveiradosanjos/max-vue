76efd167ecd5886e7436c4ca87b5d7a69b2e4656
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-06-09 21:34:27 -0300

    126. Passing Content with Slots

---
+++ vuejs-complete/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-quote>
+      <h2>The Quote</h2>
+      <p>A wonderful Quote</p>
+    </app-quote>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+
+export default {
+  name: "App",
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs-complete/src/components/Quote.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
+</style>
