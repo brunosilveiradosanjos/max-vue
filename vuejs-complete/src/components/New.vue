6d7349d2734a591d1e0c6c17d25069f4062853fb
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-06-11 11:30:58 -0300

    133. Understanding Dynamic Component Behavior

--- vuejs-complete/src/components/New.vue
+++ vuejs-complete/src/components/New.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h3>New Quote</h3>
+    <button @click="counter++">Counter</button>
+    <p>{{counter}}</p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  //   By default the component will be destroyed when other were selected
+  destroyed() {
+    console.log("New was destroyed");
+  }
+};
+</script>
