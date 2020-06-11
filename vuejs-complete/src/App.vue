576040d82c02719ff267d864abbb871bdf6d14a0
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-06-11 00:16:18 -0300

    132. Switching Multiple Components with Dynamic Co

--- vuejs-complete/src/App.vue
+++ vuejs-complete/src/App.vue
@@ -1,24 +1,43 @@
 <template>
-  <div id="app">
-    <app-quote>
-      <h2 slot="title">{{quoteTitle}}</h2>
-      <p>A wonderful Quote</p>
-    </app-quote>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <p>{{selectedComponent}}</p>
+        <hr />
+        <div v-if="selectedComponent != 'appQuote'">
+          <component :is="selectedComponent"></component>
+        </div>
+        <div v-if="selectedComponent=='appQuote'">
+          <app-quote>
+            <h2 slot="title">{{quoteTitle}}</h2>
+            <p>A wonderful Quote</p>
+          </app-quote>
+        </div>
+      </div>
+    </div>
     <img alt="Vue logo" src="./assets/logo.png" />
   </div>
 </template>
 
 <script>
 import Quote from "./components/Quote.vue";
+import Author from "./components/Author.vue";
+import New from "./components/New.vue";
 
 export default {
   data() {
     return {
-      quoteTitle: "The Quote"
+      quoteTitle: "The Quote",
+      selectedComponent: "appQuote"
     };
   },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
 };
 </script>
