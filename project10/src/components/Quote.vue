2cbca28c6ee4814f1b44cef9403f5781c8a942b6
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-06-25 01:32:50 -0300

    143. Passing Data with Props and Slots

---
+++ project10/src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ma-2 pa-2">
+    <v-btn x-large class="grey lighten-2">
+      <div class="panel-body">
+        <slot></slot>
+      </div>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quotes"]
+};
+</script>
+
+
+<style scoped>
+.panel-body {
+  font-family: "Arizonia", cursive;
+  text-transform: lowercase;
+  font-size: 30px;
+}
+/* .quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: #ffe2e2;
+} */
+</style>
---
+++ project10/src/components/QuoteGrid.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <app-quote v-for="quote in quotes" :key="quote">{{quote}}</app-quote>
+  </div>
+</template>
+
+
+<script>
+import Quote from "./Quote";
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
