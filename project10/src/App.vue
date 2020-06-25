85dec542fd630996f6f55563b994e8aefe3df61c
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-06-24 22:53:31 -0300

    139. Module Introduction

---
+++ project10/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>funciona</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10
+    };
+  }
+};
+</script>
+
+<style>
+</style>
